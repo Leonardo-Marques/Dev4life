6bacdce405ef5110580d5acb25d3a47fffaa16b5
Mathias Voelcker <mathias_V94@hotmail.com>
2017-10-28 23:16:58 -0200

    adicionar longestTimeSpentLiving

--- controllers/tipsController.js
+++ controllers/tipsController.js
@@ -1,34 +1,38 @@
 angular.module('app').controller('tipsController', function ($scope, $rootScope, $routeParams, $http, $location) {
 
-    if ($rootScope.summoner === undefined) {
-        $location.path("/");
+  if ($rootScope.summoner === undefined) {
+    $location.path("/");
+  }
+
+  $scope.matchId = $routeParams.idMatch
+  $scope.summoner = $rootScope.summoner;
+
+  $http.get("http://localhost:4040/match/" + $scope.matchId)
+  .then(function (response) {
+    $scope.match = response.data;
+    $scope.match.participantIdentities.forEach(function (participant) {
+      console.log("api name: " + participant.player.summonerName)
+      console.log("this name: " + $scope.summoner)
+      if ($scope.summoner !== undefined && participant.player.summonerName.toUpperCase() == $scope.summoner.toUpperCase()) {
+        $scope.participantId = participant.participantId
+      }
+    })
+    $scope.gameMode = $scope.match.gameMode
+  })
+
+  $scope.setLongestTimeSpentLiving = function(){
+    var longestTimeSpentLiving = $scope.match.participants[$scope.participantId - 1].stats.longestTimeSpentLiving
+    return
+  }
+
+  $scope.setFarmRatio = function(){
+    var creepHash = $scope.match.participants[$scope.participantId - 1].timeline.creepsPerMinDeltas;
+    var creepRatio = 0;
+    for(var creepKey in creepHash){
+      creepRatio = creepRatio + creepHash[creepKey];
     }
-
-    $scope.matchId = $routeParams.idMatch
-    $scope.summoner = $rootScope.summoner;
-
-    $http.get("http://localhost:4040/match/" + $scope.matchId)
-        .then(function (response) {
-            $scope.match = response.data;
-            $scope.match.participantIdentities.forEach(function (participant) {
-                console.log("api name: " + participant.player.summonerName)
-                console.log("this name: " + $scope.summoner)
-                if ($scope.summoner !== undefined && participant.player.summonerName.toUpperCase() == $scope.summoner.toUpperCase()) {
-                    $scope.participantId = participant.participantId
-                }
-            })
-            $scope.gameMode = $scope.match.gameMode
-
-            $scope.setFarmRatio = function(){
-                var creepHash = $scope.match.participants[8].timeline.creepsPerMinDeltas;
-                var creepRatio = 0;
-                for(var creepKey in creepHash){
-                  creepRatio = creepRatio + creepHash[creepKey];
-                }
-                creepRatio = Number(creepRatio/4).toFixed(2)
-                return creepRatio;
-            }
-        })
-
+    creepRatio = Number(creepRatio/4).toFixed(2)
+    return creepRatio;
+  }
 
 });
